384f99ffd70d4f5138c09e7f6c8c023c7f5621fb
Huygens Adrien <adrien.huygens@gmail.com>
2019-11-14 16:49:27 +0100

    correction à faire

--- exercices/serie1/ex13.py
+++ exercices/serie1/ex13.py
@@ -4,9 +4,9 @@ nb_batiments = int(input("Combien de bâtiment(s) à contruire ? : "))
 ressources_necessaires = int(input("Combien de ressources nécessaires par bâtiment ? : "))
 ressources_disponibles = int(input("Combien de ressources possèdons-nous ? : "))
 
-nb_batiments_valide = nb_batiments != 0 and nb_batiments <= 15
-ressources_insuffisantes = nb_batiments * ressources_necessaires > ressources_disponibles 
-peut_construire =  nb_batiments != 0 and nb_batiments <= 15 and nb_batiments * ressources_necessaires <= ressources_disponibles
+nb_batiments_valide = nb_batiments != 0 and nb_batiments <= 15 #si cela vaux -1 cela passe
+ressources_insuffisantes = nb_batiments * ressources_necessaires > ressources_disponibles # si le nombre de ressource est exactement le meme que ce que nous avons besoin on doit pouvoir construire
+peut_construire =  nb_batiments != 0 and nb_batiments <= 15 and nb_batiments * ressources_necessaires <= ressources_disponibles # tu as déja virifier les condition et tu refais la même chose, utilise les variables déjà défini.
 
 print("\nLe nombre de bâtiment est-il valide ? : {}".format(nb_batiments_valide))
 print("Les ressources sont-elles insuffisantes ? : {}".format(ressources_insuffisantes))
