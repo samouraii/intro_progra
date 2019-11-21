400d16510e9433933a69c5b52208a3f52eb82c21
Huygens Adrien <adrien.huygens@gmail.com>
2019-11-21 16:57:16 +0100

    correction

--- exercices/serie2/ex17.py
+++ exercices/serie2/ex17.py
@@ -2,9 +2,9 @@
 
 ### FONCTION ###
 def affiche_echiquier():
-    for i in range(8):
+    for i in range(8): #pas de range et pas vue les for ....
         if i %2 == 0:
-            print("_ # _ # _ # _ #")
+            print("_ # _ # _ # _ #") # "_#" * 4
         else: 
             print("# _ # _ # _ # _")
 
